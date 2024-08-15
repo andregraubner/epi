b2ab6db3f6203902993dbda3396cebc3fcee0b26
Andre <andre.graubner@web.de>
2024-08-15 17:31:23 +0800

    Decompress reference genome in download file

--- utils/download_data.sh
+++ utils/download_data.sh
@@ -15,8 +15,9 @@ mkdir -p "$DESTINATION_FOLDER"
 # Download methylation data
 wget -P "$DESTINATION_FOLDER" "https://download.cncb.ac.cn/scmethmap/singlebed/oocyte_Human.tar.gz"
 
-# Download reference genome data
+# Download and unzip reference genome data
 wget -P "$DESTINATION_FOLDER" "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz"
+gzip -d "$DESTINATION_FOLDER/hg38.fa.gz"
 
 # Get the absolute path of the current bash script
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
